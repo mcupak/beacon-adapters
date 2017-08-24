55417c5689e607f4b25dbe98ab4e8ee351d8dfdb
epidemia <exclusive_pro@mail.ru>
2017-08-24 12:32:15 +0300

    Add new beacon adapter for fhir

--- beacon-adapter-fhir/src/main/java/com/dnastack/beacon/adapter/variants/VariantsBeaconAdapter.java
+++ beacon-adapter-fhir/src/main/java/com/dnastack/beacon/adapter/variants/VariantsBeaconAdapter.java
@@ -1,13 +1,23 @@
 package com.dnastack.beacon.adapter.variants;
 
 import com.dnastack.beacon.adapter.api.BeaconAdapter;
+import com.dnastack.beacon.adapter.variants.client.fhir.FhirClient;
 import com.dnastack.beacon.exceptions.BeaconException;
 import com.dnastack.beacon.utils.AdapterConfig;
+import com.dnastack.beacon.utils.ConfigValue;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.ga4gh.beacon.Beacon;
 import org.ga4gh.beacon.BeaconAlleleRequest;
 import org.ga4gh.beacon.BeaconAlleleResponse;
+import org.ga4gh.beacon.BeaconDatasetAlleleResponse;
+import org.hl7.fhir.dstu3.model.Sequence;
 
 import javax.enterprise.context.Dependent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -16,26 +26,115 @@ import java.util.List;
 @Dependent
 public class VariantsBeaconAdapter implements BeaconAdapter {
 
-    @Override
-    public void initAdapter(AdapterConfig adapterConfig) {
+    private Beacon beacon;
+    private FhirClient fhirClient;
 
+    private void checkAdapterInit() {
+        if (fhirClient == null) {
+            throw new IllegalStateException("VariantsBeaconAdapter adapter has not been initialized");
+        }
+    }
+
+    private void initGa4ghClient(AdapterConfig adapterConfig) {
+        List<ConfigValue> configValues = adapterConfig.getConfigValues();
+        String url = null;
+
+        for (ConfigValue configValue : configValues) {
+            switch (configValue.getName()) {
+                case "beaconJsonFile":
+                    beacon = readBeaconJsonFile(configValue.getValue());
+                    break;
+                case "beaconJson":
+                    beacon = readBeaconJson(configValue.getValue());
+                    break;
+                case "url":
+                    url = configValue.getValue();
+                    break;
+            }
+        }
+
+        if (beacon == null) {
+            throw new RuntimeException(
+                    "Missing required parameter: beaconJson. Please add the appropriate configuration parameter then retry");
+        }
+
+        fhirClient = new FhirClient(url);
+    }
+
+    private Beacon readBeaconJsonFile(String filename) {
+        File beaconJsonFile = new File(filename);
+        if (!beaconJsonFile.exists()) {
+            throw new RuntimeException("BeaconJson file does not exist");
+        }
+        try {
+
+            String beaconJson = new String(Files.readAllBytes(beaconJsonFile.toPath()));
+            return readBeaconJson(beaconJson);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private Beacon readBeaconJson(String json) {
+        Gson gson = new GsonBuilder().create();
+        return gson.fromJson(json, Beacon.class);
+    }
+
+    private BeaconAlleleRequest createRequest(String referenceName, Long start, String referenceBases, String alternateBases,
+                                              String assemblyId, List<String> datasetIds, Boolean includeDatasetResponses) {
+        return BeaconAlleleRequest.newBuilder()
+                .setReferenceName(referenceName)
+                .setStart(start)
+                .setReferenceBases(referenceBases)
+                .setAlternateBases(alternateBases)
+                .setAssemblyId(assemblyId)
+                .setDatasetIds(datasetIds)
+                .setIncludeDatasetResponses(includeDatasetResponses)
+                .build();
     }
 
     @Override
-    public BeaconAlleleResponse getBeaconAlleleResponse(BeaconAlleleRequest beaconAlleleRequest) throws BeaconException {
-        return null;
+    public void initAdapter(AdapterConfig adapterConfig) {
+        initGa4ghClient(adapterConfig);
+    }
+
+    @Override
+    public BeaconAlleleResponse getBeaconAlleleResponse(BeaconAlleleRequest request) throws BeaconException {
+        checkAdapterInit();
+
+        List<Sequence.SequenceVariantComponent> variants = fhirClient.getVariants(request.getStart(), request.getStart() + 1,
+                request.getReferenceBases(), request.getAlternateBases());
+
+        return BeaconAlleleResponse.newBuilder()
+                .setBeaconId(beacon.getId())
+                .setError(null)
+                .setAlleleRequest(request)
+                .setExists(!variants.isEmpty())
+                .setDatasetAlleleResponses(Collections.singletonList(BeaconDatasetAlleleResponse.newBuilder()
+                        .setExists(!variants.isEmpty())
+                        .build()))
+                .build();
     }
 
     @Override
     public BeaconAlleleResponse getBeaconAlleleResponse(String referenceName, Long start, String referenceBases,
                                                         String alternateBases, String assemblyId, List<String> datasetIds,
                                                         Boolean includeDatasetResponses) throws BeaconException {
-        return null;
+        checkAdapterInit();
+        BeaconAlleleRequest request = createRequest(referenceName,
+                start,
+                referenceBases,
+                alternateBases,
+                assemblyId,
+                datasetIds,
+                includeDatasetResponses);
+        return getBeaconAlleleResponse(request);
     }
 
     @Override
     public Beacon getBeacon() throws BeaconException {
-        return null;
+        return beacon;
     }
 
 }
--- beacon-adapter-fhir/src/main/java/com/dnastack/beacon/adapter/variants/client/fhir/FhirClient.java
+++ beacon-adapter-fhir/src/main/java/com/dnastack/beacon/adapter/variants/client/fhir/FhirClient.java
@@ -1,7 +1,54 @@
 package com.dnastack.beacon.adapter.variants.client.fhir;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.IGenericClient;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Sequence;
+import org.hl7.fhir.dstu3.model.Sequence.SequenceVariantComponent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author Andrey Mochalov (mochalovandrey@gmail.com)
  */
 public class FhirClient {
+
+    private IGenericClient client;
+
+    public FhirClient(String url) {
+        FhirContext fhirContext = FhirContext.forDstu3();
+
+        client = fhirContext.newRestfulGenericClient(url);
+    }
+
+    public List<SequenceVariantComponent> getVariants(Long start, Long end, String refAllele, String obsAllele) {
+        Bundle results = client
+                .search()
+                .forResource(Sequence.class)
+                .returnBundle(Bundle.class)
+                .execute();
+
+        List<SequenceVariantComponent> variants = new ArrayList<>();
+
+        for (Bundle.BundleEntryComponent bundleEntryComponent : results.getEntry()) {
+            Sequence sequence = (Sequence) bundleEntryComponent.getResource();
+
+            variants.addAll(sequence.getVariant().stream()
+                    .filter(sequenceVariantComponent -> filterVariants(start, end, refAllele, obsAllele, sequenceVariantComponent))
+                    .collect(Collectors.toList()));
+        }
+
+        return variants;
+
+    }
+
+    private boolean filterVariants(Long start, Long end, String refAllele, String obsAllele, SequenceVariantComponent sequenceVariantComponent) {
+        return sequenceVariantComponent.getStart() == start
+                && sequenceVariantComponent.getEnd() == end
+                && sequenceVariantComponent.getReferenceAllele().equalsIgnoreCase(refAllele)
+                && sequenceVariantComponent.getObservedAllele().equalsIgnoreCase(obsAllele);
+    }
+
 }
